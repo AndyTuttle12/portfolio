353e268731128e9c026c22f64b1f8f185685b5ae
Andy Tuttle <andytuttle12@gmail.com>
2017-02-01 20:45:58 -0500

    Added positioning and parallax to portfolio.

--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -2,13 +2,52 @@ import React, { Component } from 'react';
 import '../styles.css';
 
 class MainBody extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			pos: {x: 500, y: 500}
+		};
+		this.onMouseMove = this.onMouseMove.bind(this);
+	}
+
+	onMouseMove(event){
+		console.log(this.state.pos);
+		this.setState({
+			pos: {
+				x: event.pageX,
+				y: event.pageY
+			}
+		});
+		this.parallax();
+	}
+
+	parallax(){
+		if ('ontouchstart' in window == false) {
+			console.log(this.refs.section)
+			let [moveX, moveY] = [(this.state.pos.x / -30), (this.state.pos.y / -40)];
+			let [section, l1, l2, l3, l4, l5, foreground] = [
+				this.refs.section, 
+				this.refs.l1, 
+				this.refs.l2,
+				this.refs.l3,
+				this.refs.l4,
+				this.refs.l5,
+				this.refs.foreground
+			];
+			foreground.style.transform = `translate3d(${0}px, ${0}px, 0)`;
+			l1.style.transform = `translate3d(${moveX * -.2}px, ${moveY * -.2}px, 0)`;
+			l2.style.transform = `translate3d(${moveX * -.5}px, ${moveY * -.5}px, 0)`;
+			l3.style.transform = `translate3d(${moveX * -.8}px, ${moveY * -.8}px, 0)`;
+			l4.style.transform = `translate3d(${moveX * -1}px, ${moveY * -1}px, 0)`;
+			l5.style.transform = `translate3d(${moveX * -.7}px, ${moveY * -.7}px, 0)`;
+			section.style.transform = `translate3d(${0}px, ${0}px, 0)`;
+		}
+	}
 
 	render() {
-		// var scene = document.getElementById('scene');
-		// var parallax = new Parallax(scene);
 		return (
 			<div className="MainRoutes">
-				<div className="centered">
+				<div className="centered" onMouseMove={this.onMouseMove} ref="foreground">
 					<div className="hero">
 						<h1>Hi, I&#39;m Andy.</h1>
 					</div>
@@ -19,12 +58,12 @@ class MainBody extends Component {
 						<h3>Please look around...</h3>
 					</div>
 				</div>
-				<div className="parallax-bg">
-						<img className="l1" role="presentation" src={require('../layer1.png')}/>
-						<img className="l2" role="presentation" src={require("../layer2.png")}/>
-						<img className="l3" role="presentation" src={require("../layer3.png")}/>
-						<img className="l4" role="presentation" src={require("../layer4.png")}/>
-						<img className="l5" role="presentation" src={require("../layer5.png")}/>
+				<div className="parallax-bg" ref="section" onMouseMove={this.onMouseMove}>
+					<img className="l1" ref="l1" role="presentation" onMouseMove={this.onMouseMove} src={require("../layer1.png")}/>
+					<img className="l2" ref="l2" role="presentation" onMouseMove={this.onMouseMove} src={require("../layer2.png")}/>
+					<img className="l3" ref="l3" role="presentation" onMouseMove={this.onMouseMove} src={require("../layer3.png")}/>
+					<img className="l4" ref="l4" role="presentation" onMouseMove={this.onMouseMove} src={require("../layer4.png")}/>
+					<img className="l5" ref="l5" role="presentation" onMouseMove={this.onMouseMove} src={require("../layer5.png")}/>
 				</div>
 			</div>
 		)
