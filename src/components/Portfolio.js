4cbe8737b1e9703441c9b6c407c9de1f6215e793
Andy Tuttle <andytuttle12@gmail.com>
2017-03-25 14:31:23 -0400

    Fixed screenshot of canigo.

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -15,7 +15,7 @@ class Portfolio extends Component {
 				</div>
 				<div className="feature projects">
 					<a href="http://www.canigo.co" target="_blank"><h2>CaniGo.co</h2></a>
-					<a href="http://www.canigo.co" target="_blank"><img src="https://camo.githubusercontent.com/2f0155fc2f452fbe247fe3c2cb82b38017dda740/687474703a2f2f692e696d6775722e636f6d2f33454e7079447a2e706e67" alt="canigo.co"/></a>
+					<a href="http://www.canigo.co" target="_blank"><img src={require('../canigoscreenshot.png')} alt="canigo.co"/></a>
 					<div className="overlay">
 						<div className="arrow"></div>
 						<h3>Project overview:</h3>
