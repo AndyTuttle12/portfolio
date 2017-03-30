14a7532f4a79dff2899ee49139c5c2e85424e3ce
Andy Tuttle <andytuttle12@gmail.com>
2017-03-29 23:47:28 -0400

    Updated portfolio with movie app project.

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -24,6 +24,17 @@ class Portfolio extends Component {
 						<p>React.js, Redux, jQuery, Node.js, Express, MySQL, Amadeus API, Stripe API, HTML/CSS, Sass and Javascript ES6</p>
 					</div>
 				</div>
+				<div className="feature projects">
+					<a href="http://andytuttle.io/movie-time" target="_blank"><h2>Movie Time</h2></a>
+					<a href="http://andytuttle.io/movie-time" target="_blank"><img src={require('../browsing.png')} alt="Movie Time"/></a>
+					<div className="overlay">
+						<div className="arrow"></div>
+						<h3>Project overview:</h3>
+						<p>Movie Time is a simple app where the user can browse titles in various categories, search, filter and sort to find new and interesting movies. The whole app is very fast light-weight, even though all of the data is real and coming from a live API.</p>
+						<h3>Technology used:</h3>
+						<p>HTML/CSS, Bootstrap, Javascript, jQuery, The Movie Database API and YouTube Player iframe API</p>
+					</div>
+				</div>
 			</div>
 		)
 	}
